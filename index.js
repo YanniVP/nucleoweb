74fe8286ef0e85c170f4ca6a642417308ce6228e
unknown <yannivegaperez@gmail.com>
2022-04-25 21:39:38 +0400

    firebase file + sdk firebase en index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfaYv-q1EDJwFxwwdsXElD7OFLCgQiKPc",
+  authDomain: "nucleoweb-842b9.firebaseapp.com",
+  databaseURL: "https://nucleoweb-842b9-default-rtdb.firebaseio.com",
+  projectId: "nucleoweb-842b9",
+  storageBucket: "nucleoweb-842b9.appspot.com",
+  messagingSenderId: "930079540705",
+  appId: "1:930079540705:web:9912bac3678960a3783ea0",
+  measurementId: "G-X1VLBLLEC0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
